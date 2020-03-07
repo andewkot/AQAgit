3a30d9fc197e5ef4fc7cd293e867c169333f809b
Andre Kot <andrii.bekey@gmail.com>
2020-03-07 11:30:11 +0300

    Test25

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -15,5 +15,9 @@ public class Main {
         System.out.println("slo");
         System.out.println("slo");
         System.out.println("slo");
+        System.out.println("slo");
+        System.out.println("slo");
+        System.out.println("slo");
+
     }
 }
